1c10e2284234b000eb858b886d957c27afe71e84
yanci <799433031@qq.com>
2021-05-09 18:17:52 +0800

    jdk8

---
+++ src/test/java/HashMapTest.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        Map<String,Double> hashMap = new HashMap<>();
+
+        hashMap.put( "k1", 0.1 );
+        hashMap.put( "k2", 0.2 );
+        hashMap.put( "k3", 0.3 );
+        hashMap.put( "k4", 0.4 );
+
+        for ( Map.Entry<String,Double> entry : hashMap.entrySet() ) {
+            System.out.println( entry.getKey() +"：" + entry.getValue());
+        }
+
+    }
+}
